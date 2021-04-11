f4c759e3976692303b16b01816102791138ff0ef
alvdelci <delcimariosantos@gmail.com>
2021-04-11 10:58:16 -0300

    Adicionando lista de a fazeres e animações de entrada

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import TaskList from './src/components/TaskList';
+import * as Animatable from 'react-native-animatable';
+
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
+
+  const [task, setTask] = useState([
+    { key: 1, task: "Comprar pão" },
+    { key: 2, task: "Comprar manteiga" },
+    { key: 3, task: "Comprar café" },
+    { key: 4, task: "Comprar leite" },
+    { key: 5, task: "Comprar torrada" },
+  ]);
+  const [open, setOpen] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#171d31" barStyle="light-content"></StatusBar>
+
+      <View style={styles.content}>
+        <Text style={styles.title}>Minhas Tarefas</Text>
+      </View>
+
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={(item) => String(item.key)}
+        renderItem={({ item }) => <TaskList data={item} />}
+      />
+
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <SafeAreaView>
+          <Text>texto</Text>
+        </SafeAreaView>
+      </Modal>
+
+
+      <AnimatedBtn
+        style={styles.fab}
+        useNativeDriver
+        animation="bounceIn"
+        duration={1500}
+        onPress={() => setOpen(true)}
+      >
+        <Ionicons name="ios-add" size={35} color="#fff" />
+      </AnimatedBtn>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171d31',
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+    color: "#fff"
+  },
+
+  fab: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#0094ff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3
+    }
+  }
 });
