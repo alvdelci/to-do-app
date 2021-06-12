06e799d8ad0cfc11df99aa98aead6e6e790fe512
alvdelci <delcimariosantos@gmail.com>
2021-06-12 02:00:43 -0300

    Adicionando nova tela de exibição de tarefas

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-community/async-storage'
 import { Ionicons } from '@expo/vector-icons';
 import TaskList from './src/components/TaskList';
 import * as Animatable from 'react-native-animatable';
+import CheckList from './src/components/CheckList';
 
 const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
@@ -11,7 +12,9 @@ export default function App() {
 
   const [task, setTask] = useState([]);
   const [open, setOpen] = useState(false);
+  const [checkOpen, setCheckOpen] = useState(false);
   const [input, setInput] = useState('');
+  const [checkedTasks, setCheckedTasks] = useState([]);
 
   //Buscando todas as tarefas ao iniciar o app
   useEffect(() => {
@@ -36,6 +39,17 @@ export default function App() {
 
   }, [task]);
 
+  //Salvando tarefas concluídas
+  useEffect(() => {
+
+    async function saveCheckedTasks() {
+      await AsyncStorage.setItem('@task', JSON.stringify(checkedTasks));
+    }
+
+    saveCheckedTasks()
+
+  }, [checkedTasks]);
+
   function handleAdd() {
     if (input === '') return;
 
@@ -52,11 +66,25 @@ export default function App() {
   const handleDelete = useCallback((data) => {
     const find = task.filter(r => r.key !== data.key);
     setTask(find);
+    const removed = task.filter(r => r.key === data.key);
+    setCheckedTasks(removed);
   });
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#171d31" barStyle="light-content"></StatusBar>
+
+      {/** Botão visualizar tarefas concluídas */}
+      <AnimatedBtn
+        style={styles.checked}
+        useNativeDriver
+        animation="bounceIn"
+        duration={1500}
+        onPress={() => setCheckOpen(true)}
+      >
+        <Ionicons name="md-checkmark-circle" size={25} color="#fff" />
+      </AnimatedBtn>
+
 
       {/** Tela inicial de exibição das tasks */}
       <View style={styles.content}>
@@ -86,7 +114,6 @@ export default function App() {
             <TextInput
               multiline={true}
               placeholderTextColor="#747474"
-              autoCorrect={false}
               placeholder="Ex: Estudar algo novo."
               style={styles.input}
               value={input}
@@ -100,6 +127,35 @@ export default function App() {
         </SafeAreaView>
       </Modal>
 
+      {/** Tela de exibição de tarefas concluídas */}
+
+      <Modal animationType="slide" transparent={false} visible={checkOpen}>
+        <SafeAreaView style={styles.modal}>
+
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress={() => setCheckOpen(false)}>
+              <Ionicons style={{ marginLeft: 5, marginRight: 5 }} name="md-arrow-back" size={30} color="#fff" />
+            </TouchableOpacity>
+            <Text style={styles.modalTitle}>Tarefas Concluídas</Text>
+          </View>
+
+          <Animatable.View style={styles.modalBody} animation="fadeInUp">
+            <View style={styles.content}>
+              <Text style={styles.title}>Estamos a todo vapor!!!</Text>
+            </View>
+            {/**Lista de Tarefas concluídas*/}
+            <FlatList
+              marginHorizontal={10}
+              showsHorizontalScrollIndicator={false}
+              data={checkedTasks}
+              keyExtractor={(item) => String(item.key)}
+              renderItem={({ item }) => <CheckList data={item} />}
+            />
+          </Animatable.View>
+        </SafeAreaView>
+      </Modal>
+
+
       <AnimatedBtn
         style={styles.fab}
         useNativeDriver
@@ -107,7 +163,7 @@ export default function App() {
         duration={1500}
         onPress={() => setOpen(true)}
       >
-        <Ionicons name="ios-add" size={35} color="#fff" />
+        <Ionicons name="ios-add" size={25} color="#fff" />
       </AnimatedBtn>
 
     </SafeAreaView>
@@ -129,14 +185,34 @@ const styles = StyleSheet.create({
 
   fab: {
     position: 'absolute',
-    width: 60,
-    height: 60,
+    width: 45,
+    height: 45,
     backgroundColor: '#0094ff',
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 30,
-    right: 25,
-    bottom: 25,
+    right: 10,
+    bottom: 10,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3
+    }
+  },
+
+  checked: {
+    position: 'absolute',
+    width: 45,
+    height: 45,
+    backgroundColor: '#0094ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    left: 10,
+    bottom: 10,
     elevation: 2,
     zIndex: 9,
     shadowColor: '#000',
@@ -163,6 +239,7 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     fontSize: 23,
     color: '#fff',
+    marginLeft: 65
   },
 
   modalBody: {
